afef4032d27b8635475a8b5cc552500ccf527cda
mariebosq <marie-bosq-oc@protonmail.com>
2021-09-15 18:02:10 +0200

    Initialize first page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    
+    /**
+     * @Route("/", name="Home Page")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
